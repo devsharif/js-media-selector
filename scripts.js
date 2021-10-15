a3b3b18daa4c2fe76d2a4797c8827602122267d7
Sharif Ullah <dev.sharifullah@gmail.com>
2021-10-16 01:07:50 +0600

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,47 @@
+jQuery(function ($) {
+    var mediaArray = [];
+    var selectedMediasId;
+    var isMultipleAllowed = false;
+    $('#allowmultiple').click(function () {
+        isMultipleAllowed = $('#allowmultiple').is(':checked') ? true : false;
+        $('.image-checkbox-checked').each(function () {
+            $(this).removeClass('image-checkbox-checked');
+        });
+        mediaArray = [];
+        $('#selectedmediapreview').html('');
+    });
+
+
+    $(".image-checkbox").on("click", function (e) {
+        var selected = $(this).find('img').attr('su-media-id');
+        //console.log(selected);
+        if ($(this).hasClass('image-checkbox-checked')) {
+            $(this).removeClass('image-checkbox-checked');
+            // remove deselected item from array
+            mediaArray = $.grep(mediaArray, function (value) {
+                return value != selected;
+            });
+        }
+        else {
+            if (isMultipleAllowed == false) {
+                $('.image-checkbox-checked').each(function () {
+                    $(this).removeClass('image-checkbox-checked');
+                });
+                mediaArray = [];
+                mediaArray.push(selected);
+            } else {
+                if (mediaArray.indexOf(selected) === -1) {
+                    mediaArray.push(selected);
+                }
+            }
+            $(this).addClass('image-checkbox-checked');
+        }
+        //console.log(selected);
+        console.log(mediaArray);
+        selectedMediasId = mediaArray.join(",");
+        console.log(selectedMediasId);
+        $('#selectedmediapreview').html('<div class="alert alert-success"><pre lang="js">' + JSON.stringify(mediaArray.join(", "), null, 4) + '</pre></div>');
+        //console.log(isMultipleAllowed);
+        e.preventDefault();
+    });
+});
